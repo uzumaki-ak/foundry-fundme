4ea628ce935a0bb9411138191ed6750c31545ca1
uzumaki-ak <anikydv38@gmail.com>
2026-03-10 20:29:23 +0000

    made code modular by removing hardcoded aggregator add

--- script/DeployFundme.s.sol
+++ script/DeployFundme.s.sol
@@ -5,9 +5,10 @@ import {Script, console} from "forge-std/Script.sol";
 import {FundMe} from "../src/Fundme.sol";
 
 contract DeployFundme is Script {
-    function run() external {
+    function run() external returns (FundMe) {
         vm.startBroadcast();
-        new FundMe();
+        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         vm.stopBroadcast();
+        return fundMe;
     }
 }
--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -3,21 +3,30 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/Fundme.sol";
+import {DeployFundme} from "../script/DeployFundme.s.sol";
 
 contract FundmeTest is Test {
     FundMe fundMe;
 
-    function setUp() public {
-        fundMe = new FundMe();
+    function setUp() external {
+        // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundme deployFundMe = new DeployFundme();
+        fundMe = deployFundMe.run();
     }
 
     function testMinimumFiveUsd() public {
         assertEq(fundMe.MINIMUM_USD(), 5 * 10 ** 18);
     }
-    function testOwnerIsMessageSender()public {
+
+    function testOwnerIsMessageSender() public {
         // console.log(fundMe.i_owner());
         // console.log(msg.sender);
         // assertEq(fundMe.i_owner(), msg.sender);
-        assertEq(fundMe.i_owner(),address(this));
+        assertEq(fundMe.getOwner(), address(this));
+    }
+
+    function testPriceFeedVersion() public {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
     }
 }
