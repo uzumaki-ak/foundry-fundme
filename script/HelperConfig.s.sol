2d83ba48464b2d5fbae64ba200a5c92af857e257
uzumaki-ak <anikydv38@gmail.com>
2026-03-11 20:33:23 +0000

    removed magic no. with type safe

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mock/MockV3Aggregator.sol";
 
 contract HelperConfig is Script {
     //defining a type using struct, this is a custom type that we can use to group together related data. In this case, we're defining a struct called NetworkConfig that has a single field called priceFeed, which is an address. This struct will be used to store the configuration for different networks, such as the address of the price feed contract on each network.
@@ -11,6 +12,8 @@ contract HelperConfig is Script {
     }
 
     NetworkConfig public activeNetworkConfig;
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
 
     // we want to set the activeNetworkConfig variable to the appropriate configuration based on the chain ID of the network we're deploying to. The constructor checks the chain ID using block.chainid and sets activeNetworkConfig to the appropriate configuration for either the Sepolia testnet or a local Anvil environment.
 
@@ -31,9 +34,12 @@ contract HelperConfig is Script {
     }
 
     function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
-        // For local Anvil testing, we'll use a mock price feed address (this will be replaced in tests)
+        vm.startBroadcast();
+            MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
+        vm.stopBroadcast();
+
         NetworkConfig memory anvilConfig = NetworkConfig({
-            priceFeed: 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38
+            priceFeed: address(mockPriceFeed)
         });
         return anvilConfig;
     }
--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -7,26 +7,29 @@ import {DeployFundme} from "../script/DeployFundme.s.sol";
 
 contract FundmeTest is Test {
     FundMe fundMe;
+    address deployer;
 
     function setUp() external {
         // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         DeployFundme deployFundMe = new DeployFundme();
         fundMe = deployFundMe.run();
+        deployer = vm.addr(vm.envUint("PRIVATE_KEY"));
     }
 
-    function testMinimumFiveUsd() public {
+    function testMinimumFiveUsd() public view {
         assertEq(fundMe.MINIMUM_USD(), 5 * 10 ** 18);
     }
 
-    function testOwnerIsMessageSender() public {
+    function testOwnerIsMessageSender() public view {
         // console.log(fundMe.i_owner());
         // console.log(msg.sender);
         // assertEq(fundMe.i_owner(), msg.sender);
-        assertEq(fundMe.getOwner(), address(this));
+        assertEq(fundMe.getOwner(), deployer);
     }
 
-    function testPriceFeedVersion() public {
+    function testPriceFeedVersion() public view {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
     }
 }
+
